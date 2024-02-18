bd4df88df41afbaf94464f1de14f5b430a546906
esenturDev <nurlanuuluesentur@gmail.com>
2024-02-18 17:45:38 +0600

    added typescript is movi-list

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import scss from './Home.module.scss';
-import { NewMoviList } from './newMoviList/NewMoviList';
+import scss from "./Home.module.scss";
+import { NewMoviList } from "./newMoviList/NewMoviList";
 
 const Home = () => {
-  return (
-    <div className={scss.home}>
-      <NewMoviList/>
-    </div>
-  )
-}
+	return (
+		<div className={scss.home}>
+			<NewMoviList />
+		</div>
+	);
+};
 
-export default Home
+export default Home;
--- src/components/pages/newMoviList/NewMoviList.tsx
+++ src/components/pages/newMoviList/NewMoviList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import scss from "./NewMoviList.module.scss";
 import { MoviList } from "../../types/types";
 import axios from "axios";
